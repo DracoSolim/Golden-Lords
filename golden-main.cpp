1cb83017bdeb8df0595d476aa34959a7ced0bde5
DracoSolim <gamergabo898@gmail.com>
2023-02-23 23:17:39 -0600

    random

--- golden-main.cpp
+++ golden-main.cpp
@@ -1,3 +1,5 @@
 #include <iostream>
 
-//test
+int main(){
+
+}
---
+++ random-number-test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int random;
+    random = rand();
+
+    cout<<random<<endl;
+}
